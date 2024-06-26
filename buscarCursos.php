fcad50549774835cd1514aef1264f495d9dc3c5d
Willyam Mach <willyam.mach@gmail.com>
2024-06-26 14:37:38 -0300

    buscando cursos alura

--- buscarCursos.php
+++ buscarCursos.php
@@ -1,9 +1,20 @@
 <?php 
 
+require 'vendor\autoload.php';
+
 use \GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 $cliente = new Client();
 
-$response = $client->request(method: 'GET', 'https://cursos.alura.com.br/category/programacao#php');
+$response = $cliente->request('GET', 'https://cursos.alura.com.br/category/programacao#php');
 
 $html = $response->getBody();
+
+$crawler = new Crawler($html);
+$cursos = $crawler->filter('li.card-list__item');
+
+foreach($cursos as $key) {
+    echo $key->textContent;
+    echo "<br>";
+}
